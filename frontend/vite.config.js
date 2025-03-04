dd5934477346a67e4c6811230bbbc702fb90f53b
samahali <samahali9226@gmail.com>
2025-03-04 11:26:02 +0200

    remove / from backend url base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const API_BASE_URL = process.env.VITE_API_BASE_URL || "https://fitness-tracker-dashboard-react.onrender.com/";
+  const API_BASE_URL = process.env.VITE_API_BASE_URL || "https://fitness-tracker-dashboard-react.onrender.com";
 
   console.log("🛠 API_BASE_URL from vite.config.js:", env.VITE_API_BASE_URL);
 
