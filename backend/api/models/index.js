5645d0ecf68b4040f6791f01a33d7afbb32e080f
omnya tarek <omnya4363@gmail.com>
2025-03-26 21:59:47 +0200

    Update index.js

--- backend/api/models/index.js
+++ backend/api/models/index.js
@@ -1,9 +1,26 @@
-const User = require("./user.model");
-const Workout = require("./workout.model");
-const Goal = require("./goal.model");
-const AuthToken = require("./auth_token.model");
+/**
+ * Model Index File.
+ *
+ * This module imports and exports all Mongoose models, making it easier to 
+ * manage and use them in different parts of the application.
+ *
+ * @module models
+ *
+ * @requires ./user.model.js - User model for storing user-related data.
+ * @requires ./workout.model.js - Workout model for managing workout records.
+ * @requires ./goal.model.js - Goal model for tracking fitness goals.
+ * @requires ./auth_token.model.js - AuthToken model for handling authentication tokens.
+ *
+ * @example
+ * import { User, Workout, Goal, AuthToken } from './models/index.js';
+ * const users = await User.find();
+ */
+import User from "./user.model.js";
+import Workout from "./workout.model.js";
+import Goal from "./goal.model.js";
+import AuthToken from "./auth_token.model.js";
 
-module.exports = {
+export {
   User,
   Workout,
   Goal,
